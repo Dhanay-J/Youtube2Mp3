9938d097ab9b586f00e7ae04575e3c7178a2789a
Dhanay J <jdhanay13@gmail.com>
2024-06-01 09:35:54 +0530

    Integrated Download Object in Main

--- main.py
+++ main.py
@@ -1,91 +1,103 @@
-import time
+import threading
 from tkinter import *
+from ttkbootstrap.dialogs.dialogs import Messagebox
 from ttkbootstrap.constants import *
 import ttkbootstrap as ttk
-from pytube import YouTube
-import threading, os
-from moviepy.editor import AudioFileClip
+import os
+from pytube import Playlist
+from Download import Download
+from concurrent.futures import ThreadPoolExecutor
 
 WIDTH = 700
-HEIGHT = 500
-PROGRESS_DELAY = 0.000001
+HEIGHT = 200
 PATH = f"{os.environ.get('USERPROFILE')}\\Downloads\\Youtube2Mp3\\"
 
 if(not os.path.exists(PATH)):    
     os.makedirs(PATH)
 os.chdir(PATH)
 
-def set_download_meter(stream, chunk, bytes_remaining):
+videos = []
+MAX_THREADS = 2
+
+def download_call():
+    pool = ThreadPoolExecutor(max_workers=MAX_THREADS)
+    global videos
     
-    
-    total_size = stream.filesize
-    bytes_downloaded = total_size - bytes_remaining
-    pct_completed = int((bytes_downloaded / total_size) * 100)
-    for i in range(download_meter.amountusedvar.get(), pct_completed+1):
-        download_meter.configure(amountused=i)
-    download_meter.configure(amountused=pct_completed)
+    # Get URLs from Entry
+    urls = url_entry.get().strip().split(' ')
 
-def set_download_meter_complete(stream, file_handle):
-    for i in range(download_meter.amountusedvar.get(), 101):
-        download_meter.configure(amountused=i)
-    download_meter.configure(amountused=100)
+    # Reset GUI to empty string
+    url_entry_string.set("")
 
-    msg_label.configure(text=f"Download Completed : {stream.title}\nConverting to MP3...")
+    # Check if Playlist is Selected
+    if is_playlist_var.get():
+        # Get Playlist URL
+        playlist_url = urls[0]
+        # Get Playlist Videos
+        try:
+            p = Playlist(playlist_url)
+            for video in p.videos:
+                if video.watch_url not in videos:
+                    videos.append(video.watch_url)
+                    d = Download(video.watch_url)   
+                    pool.submit(d.download)
+        except Exception as e:
+            e = str(e)
+            if 'list' in e:
+                e = "Invalid Playlist URL"
+            elif 'urlopen' in e:
+                e = "Check Internet Connection or Firewall"
+            elif 'age rest' in e:
+                e = "Age restricted content . Cannot Download"
+            
+            Messagebox.show_error(e,"Error")
+    else:
+        # Download Video(s)        
+        for url in urls:
+            if url:
+                # To Avoid Multiple Downloads of Same URL/Audio
+                if url not in videos:
+                    videos.append(url)
+                    d = Download(url)
+                    pool.submit(d.download)
 
-    audio = AudioFileClip(file_handle)
-    audio.write_audiofile(f"{file_handle[:-4]}.mp3", verbose=False, logger=None)
-    audio.close()
-    os.remove(file_handle)
-    msg_label.configure(text=f"Download Completed : {stream.title}\nConversion Completed")
-
-def download():
-    url = url_entry.get()
-    download_meter.configure(amountused=0)
-    download_button.configure(state="disabled")
-
-    try:
-        yt = YouTube(url, on_progress_callback=set_download_meter, on_complete_callback=set_download_meter_complete)
-        msg_label.configure(text=f"Downloading : {yt.title}")
-        yt.streams.get_audio_only().download()
-    except Exception as e:
-        download_meter.configure(amountused=0)
-        
-        if "regex" in str(e):
-            msg_label.configure(text="Error : Invalid URL")
-        else:
-            msg_label.configure(text=f"Error : {str(e)}")
-
-    finally:
-        download_button.configure(state="normal")
-    # Download the video
-    # Convert the video to MP3
-    # Save the MP3 file
+    pool.shutdown()
 
 def worker():
-    thread = threading.Thread(target=download)
+    thread = threading.Thread(target=download_call)
     thread.start()
+    
 
 
 root = ttk.Window(themename="darkly")
 
 root.title("Youtube2MP3")
 root.geometry(f"{WIDTH}x{HEIGHT}")
+root.minsize(WIDTH, HEIGHT)
 
 "https://youtu.be/xEALTVLxrDw?list=RDxEALTVLxrDw"
 "https://youtu.be/WcIcVapfqXw?list=RDxEALTVLxrDw"
 "https://youtu.be/ruohUTTo8Kw" # 12 min
 "https://youtu.be/pAMiPgT1m4E" #45 min
 
-msg_label = ttk.Label(root, text="", padding=4)
+# msg_label = ttk.Label(root, text="", padding=4)
 url_label = ttk.Label(root, text="URL", padding=4)
-url_entry = ttk.Entry(root)
-download_meter = ttk.Meter(root, textright=" %",metertype="semi", metersize=180, amountused=0, bootstyle="danger")
+url_entry_string = ttk.StringVar()
+url_entry_string.set("httpe/xEALTVLxrDw?list=RDxEALTVLxrDw")
+url_entry = ttk.Entry(root, textvariable=url_entry_string)
+
+is_playlist_var = ttk.IntVar()
+is_playlist = ttk.Checkbutton(root, text="Playlist", padding=4, variable=is_playlist_var)
+
+# download_meter = ttk.Meter(root, textright=" %",metertype="semi", metersize=180, amountused=0, bootstyle="danger")
 download_button = ttk.Button(root, text="Download", style="success", padding=4,command=worker)
 
 url_label.place(relx=0.02, rely=0.1, relwidth=0.2)
 url_entry.place(relx=0.1, rely=0.1, relwidth=0.8)
-download_meter.place(relx=0.1, rely=0.3, relwidth=0.8)
+is_playlist.place(relx=0.1, rely=0.3, relwidth=0.8)
+# download_meter.place(relx=0.1, rely=0.3, relwidth=0.8)
 download_button.place(relx=0.35, rely=0.8, relwidth=0.3)
-msg_label.place(relx=0.1, rely=0.9, relwidth=0.8)
+# msg_label.place(relx=0.1, rely=0.9, relwidth=0.8)
+
 
 root.mainloop()
